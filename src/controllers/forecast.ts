89739051acd293047cbf07058bc326f04167ed62
Breno Matheus Oliveira de Jesus <brenodejesus@pop-os.localdomain>
2023-04-14 23:26:35 +0100

    project of forecast beaches

---
+++ src/controllers/forecast.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
+import { Forecast } from '@src/services/forecast';
+
+const forecast = new Forecast();
+
+@Controller('forecast')
+export class ForecastController {
+  @Get('')
+  public async getForecastForgeLoggedUser(
+    _: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const beaches = await Beach.find({});
+      const forecastData = await forecast.processForecastForBeaches(beaches);
+      res.status(200).send(forecastData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Something went wrong' });
+    }
+  }
+}
