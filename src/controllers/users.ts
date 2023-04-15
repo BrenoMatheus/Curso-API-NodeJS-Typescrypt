a4767ed7b72ae4d1920bd1aae68c0bffb4656eb7
Breno Jesus <breno.moliver@gmail.com>
2023-04-15 12:26:33 +0100

    implementation of Json Web Token JWT with Node.js for generateToken, test increment for user authentication and add auth time expired

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { BaseController } from './index';
 
 @Controller('users')
@@ -11,8 +12,29 @@ export class UsersController extends BaseController {
       const user = new User(req.body);
       const newUser = await user.save();
       res.status(201).send(newUser);
-    } catch (error:any) {
+    } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'User not found!',
+      });
+    }
+    if (
+      !(await AuthService.comparePasswords(req.body.password, user.password))
+    ) {
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+    }
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.send({ ...user.toJSON(), ...{ token } });
   }
 }
